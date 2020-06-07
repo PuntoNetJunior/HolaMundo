8f106c822fe06ddd8f140cdbc318ac040dba6960
PuntoNet Junior <puntonetjunior@gmail.com>
2020-06-07 16:16:41 -0300

    Inicial

---
+++ AgendaPersonal/Data/AgendaPersonalContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AgendaPersonal.Models;
+
+namespace AgendaPersonal.Data
+{
+    public class AgendaPersonalContext : DbContext
+    {
+        public AgendaPersonalContext (DbContextOptions<AgendaPersonalContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AgendaPersonal.Models.Persona> Persona { get; set; }
+    }
+}
---
+++ AgendaPersonal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AgendaPersonal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
